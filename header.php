f75ab3bdac14baf1eee9b47d956b454aef32f426
enzdo <enzodeniau2003@gmail.com>
2023-01-23 08:44:08 +0100

    :construction: Début du projet

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="nav-bar">
+        <div class="logo">
+            <a href="">Deniau</a>
+        </div>
+        <button></button>
+        <nav>
+            <a class="" href="">About me</a>
+            <a class="" href="">Project</a>
+            <a class="" href="">Contact</a>
+            <button class="close_button">x</button>
+        </nav>
+    </div>
+    <div class="background-header">
+        <h1>PORTFOLIO</h1>
+        <div class="dev">
+            <p>2023</p>
+            <p>developer</p>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,394 @@
+<?php
+require 'head.php';
+require 'header.php';
+?>
+
+<body>
+    <main>
+    <div class="slider">
+	<div class="slide-track">
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+	</div>
+</div>
+        <div class="aboutMe">
+            <div class="hello">
+                <img class="avatar" src="img/bg/unnamed.png" alt="">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias accusantium eaque quam repudiandae at eius, placeat temporibus voluptatem dolore error accusamus officia quae quia nihil magni illo assumenda tenetur dolorem.Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias accusantium eaque quam repudiandae at eius, placeat temporibus voluptatem dolore error accusamus officia quae quia nihil magni illo assumenda tenetur dolorem</p>
+            </div>
+            <div class="apropo">
+                <div class="container">
+                    <div class="box_1">
+                        <h3>Hardskills</h3>
+                        <div class="card">
+                            <br>
+                            <h5>Digital</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/affter.png" alt="">
+                                <img src="img/logo_software/illustrator.png" alt="">
+                                <img src="img/logo_software/indesign.png" alt="">
+                                <img src="img/logo_software/photoshop.png" alt="">
+                                <img src="img/logo_software/premiere.png" alt="">
+                                <img src="img/logo_software/xd.png" alt="">
+                                <img src="img/logo_software/figma.png" alt="">
+                                <img src="img/logo_software/blender.png" alt="">
+                                <img src="img/logo_software/3ds.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <br>
+                            <h5>Langage</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/HTML5.png" alt="">
+                                <img src="img/logo_software/css.png" alt="">
+                                <img src="img/logo_software/java.png" alt="">
+                                <img src="img/logo_software/PHP_logo.png" alt="">
+                                <img src="img/logo_software/sql.png" alt="">
+                                <img src="img/logo_software/scss.png" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <br>
+                            <h5>Framework</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/vue.png" alt="">
+                                <img src="img/logo_software/tailwind.png" alt="">
+                                <img src="img/logo_software/three.png" alt="">
+                                <img src="img/logo_software/spline.png" alt="">
+                                <img src="img/logo_software/symfony.png" alt="">
+                                <img src="img/logo_software/node.png" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <br>
+                            <h5>Cms</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/Wordpress_Blue_logo.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box_2">
+                        <div class="experiance">
+                            <h3>Education</h3>
+                            <div class="card">
+                                <h5>Étude supp</h5>
+                                <h6>IIM - Digital school axe CDI - 2021/2026</h6>
+                                <p>inserer l'adresse de l'ecole</p>
+                            </div>
+                            <div class="card">
+                                <h5>lycée</h5>
+                                <h6>Claude Garamont - 2018/2021</h6>
+                                <p>inserer l'adresse de l'ecole</p>
+                            </div>
+                            <div class="card">
+                                <h5>College</h5>
+                                <h6>Claude Garamont - 2018/2021</h6>
+                                <p>inserer l'adresse de l'ecole</p>
+                            </div>
+                        </div>
+                        <div class="experiance">
+                            <h3>Experience</h3>
+                            <div class="card">
+                                <h5>Job étudiant McDonald's</h5>
+                                <h6>Équipier polyvalent puis charger d'accueil client <br> 1 an et 4 mois - 2021/2022</h6>
+                                <p>inserer l'adresse du taff</p>
+                            </div>
+                            <div class="card">
+                                <h5>Stage</h5>
+                                <h6>Stage de 3 mois chez Axa france <br> service </h6>
+                                <p>inserer l'adresse du taff</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box_1">
+                        <h3>Softskills</h3>
+                        <div class="card">
+                            <br>
+                            <h5>Digital</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/affter.png" alt="">
+                                <img src="img/logo_software/illustrator.png" alt="">
+                                <img src="img/logo_software/indesign.png" alt="">
+                                <img src="img/logo_software/photoshop.png" alt="">
+                                <img src="img/logo_software/premiere.png" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <br>
+                            <h5>Digital</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/affter.png" alt="">
+                                <img src="img/logo_software/illustrator.png" alt="">
+                                <img src="img/logo_software/indesign.png" alt="">
+                                <img src="img/logo_software/photoshop.png" alt="">
+                                <img src="img/logo_software/premiere.png" alt="">
+                            </div>
+                        </div>
+                        <div class="card">
+                            <br>
+                            <h5>Digital</h5>
+                            <div class="softDigital">
+                                <img src="img/logo_software/affter.png" alt="">
+                                <img src="img/logo_software/illustrator.png" alt="">
+                                <img src="img/logo_software/indesign.png" alt="">
+                                <img src="img/logo_software/photoshop.png" alt="">
+                                <img src="img/logo_software/premiere.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="box_3">
+                        <h3>Contact</h3>
+                        <p><span>Numero :</span> 06 06 06 06 06</p>
+                        <p><span>Email :</span> enzo.deniaupro@gmail.com</p>
+                        <div class="passion_img">
+                                <a href=""><img src="img/logo_software/linkedIn.png" alt=""></a>
+                        </div>
+                    </div>
+                    <div class="box_3">
+                        <h3>Passions</h3>
+                        <div class="passion_img">
+                                <img src="img/logo_software/jv.png" alt="">
+                                <img src="img/logo_software/muscu.png" alt="">
+                                <img src="img/logo_software/draw.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="box_3">
+                        <h3>abilités</h3>
+                        <li>Créative</li>
+                        <li>Créative</li>
+                        <li>Créative</li>
+                        <li>Créative</li>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="slider">
+	<div class="slide-track">
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+		<div class="slide">
+			<p>get to know <span>Me</span></p>
+		</div>
+	</div>
+</div>
+        <div class="cv">
+            <Button>Cv vidéo</Button>
+            <Button>Cv papier</Button>
+        </div>
+        <div class="project">
+            <h2>My project</h2>
+            <div class="projects">
+                <div class="container_project">
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet : <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint ab saepe libero, minus commodi quidem, eum ea veniam voluptatum a officiis, ipsum ex. Nemo, ea corporis expedita saepe debitis dolores? Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique amet quo autem neque minima quaerat cum debitis beatae aliquam quibusdam! Dolorem adipisci hic omnis dignissimos odio cupiditate iure, praesentium non. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis dolore quam commodi sapiente omnis, animi quis ab delectus voluptatum? Laboriosam, dolores fuga! Maiores cupiditate iure tempora aliquid, sunt et rerum.</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container_project">
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container_project">
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container_project">
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container_project">
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                    <div class="box">
+                        <img src="img/s_n29_10229198.jpg" alt="">
+                        <div class="box-description">
+                            <div class="titre">
+                                <h4>Titre du projet</h4>
+                                <hp>Fait le : 2105</p>
+                            </div>
+                            <p>Voici une courte explication du projet</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </main>
+    <div id="box" class="box-4">
+  
+  <div class="blob blob-1"></div>
+  <div class="blob blob-2"></div>
+  <div class="blob blob-3"></div>
+  <div class="blob blob-4"></div>
+  <div class="blob blob-5"></div>
+  <div class="blob blob-6"></div>
+  <div class="blob blob-7"></div>
+  <div class="blob blob-8"></div>
+  <div class="blob blob-9"></div>
+  <div class="blob blob-10"></div>
+  <div class="blob blob-11"></div>
+  <div class="blob blob-12"></div>
+  <div class="blob blob-13"></div>
+  <div class="blob blob-14"></div>
+  <div class="blob blob-15"></div>
+  <div class="blob blob-16"></div>
+  <div class="blob blob-17"></div>
+</div>
+</body>
