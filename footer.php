51fdcd6bd502bb04049ff6bf2b1a44bba06fb935
enzdo <enzodeniau2003@gmail.com>
2023-01-27 19:50:31 +0100

    :lipstick: css

--- footer.php
+++
@@ -1,5 +0,0 @@
-<script src="script/cursor.js"> </script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js">
-</script>
-<script src="script/animation.js"></script>
-</html>
